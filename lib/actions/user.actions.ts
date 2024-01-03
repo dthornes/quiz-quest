054fdc6fc587ccae05af1ee0767391a221717313
dthornes <danielthornes@gmail.com>
2024-01-03 20:17:49 +0000

    Remove order actions

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,83 +4,59 @@ import { revalidatePath } from "next/cache";
 
 import { connectToDatabase } from "@/lib/database";
 import User from "@/lib/database/models/user.model";
-import Order from "@/lib/database/models/order.model";
-import Event from "@/lib/database/models/quiz.model";
-import { handleError } from "@/lib/utils";
+import Quiz from "@/lib/database/models/quiz.model";
 
 import { CreateUserParams, UpdateUserParams } from "@/types";
 
 export async function createUser(user: CreateUserParams) {
-	try {
-		await connectToDatabase();
+	await connectToDatabase();
 
-		const newUser = await User.create(user);
-		return JSON.parse(JSON.stringify(newUser));
-	} catch (error) {
-		handleError(error);
-	}
+	const newUser = await User.create(user);
+	return JSON.parse(JSON.stringify(newUser));
 }
 
 export async function getUserById(userId: string) {
-	try {
-		await connectToDatabase();
+	await connectToDatabase();
 
-		const user = await User.findById(userId);
+	const user = await User.findById(userId);
 
-		if (!user) throw new Error("User not found");
-		return JSON.parse(JSON.stringify(user));
-	} catch (error) {
-		handleError(error);
-	}
+	if (!user) throw new Error("User not found");
+	return JSON.parse(JSON.stringify(user));
 }
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
-	try {
-		await connectToDatabase();
+	await connectToDatabase();
 
-		const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
-			new: true,
-		});
+	const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+		new: true,
+	});
 
-		if (!updatedUser) throw new Error("User update failed");
-		return JSON.parse(JSON.stringify(updatedUser));
-	} catch (error) {
-		handleError(error);
-	}
+	if (!updatedUser) throw new Error("User update failed");
+	return JSON.parse(JSON.stringify(updatedUser));
 }
 
 export async function deleteUser(clerkId: string) {
-	try {
-		await connectToDatabase();
+	await connectToDatabase();
 
-		// Find user to delete
-		const userToDelete = await User.findOne({ clerkId });
+	// Find user to delete
+	const userToDelete = await User.findOne({ clerkId });
 
-		if (!userToDelete) {
-			throw new Error("User not found");
-		}
-
-		// Unlink relationships
-		await Promise.all([
-			// Update the 'events' collection to remove references to the user
-			Event.updateMany(
-				{ _id: { $in: userToDelete.events } },
-				{ $pull: { organiser: userToDelete._id } }
-			),
-
-			// Update the 'orders' collection to remove references to the user
-			Order.updateMany(
-				{ _id: { $in: userToDelete.orders } },
-				{ $unset: { buyer: 1 } }
-			),
-		]);
-
-		// Delete user
-		const deletedUser = await User.findByIdAndDelete(userToDelete._id);
-		revalidatePath("/");
-
-		return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-	} catch (error) {
-		handleError(error);
+	if (!userToDelete) {
+		throw new Error("User not found");
 	}
+
+	// Unlink relationships
+	await Promise.all([
+		// Update the 'events' collection to remove references to the user
+		Quiz.updateMany(
+			{ _id: { $in: userToDelete.events } },
+			{ $pull: { organiser: userToDelete._id } }
+		),
+	]);
+
+	// Delete user
+	const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+	revalidatePath("/");
+
+	return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
 }
